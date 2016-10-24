dac7e159e06ca0e99618f99cbb42cf073e49f911
AndyDunnings <andy.dunnings@btinternet.com>
2016-10-24 14:48:38 +0100

    Added HSV illumination

--- compute.py
+++ compute.py
@@ -19,54 +19,55 @@ def illumination(image, alpha):
             newImage[x][y] = newValue * 255
     return newImage
 
-windowName = "Original Image"; # window name
-windowName2 = "New Image"; # window name
+def illuminationHSV(image):
+    rows,cols,channels = image.shape
+    image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    hsv = cv2.split(image_hsv)
+    return hsv[2]
 
-# if command line arguments are provided try to read video_name
-# otherwise default to capture from attached H/W camera
+windowName = "Original Image"
+windowName2 = "New Image"
 
 if (len(sys.argv) == 2):
 
-    # create window by name (as resizable)
+    cv2.namedWindow(windowName, cv2.WINDOW_NORMAL)
+    cv2.namedWindow(windowName2, cv2.WINDOW_NORMAL)
 
-    cv2.namedWindow(windowName, cv2.WINDOW_NORMAL);
-    cv2.namedWindow(windowName2, cv2.WINDOW_NORMAL);
-
-    # add some track bar controllers for settings
-
-    lower_threshold = 25;
-    upper_threshold = 120;
-    smoothing_neighbourhood = 3;
-    sobel_size = 3; # greater than 7 seems to crash
+    lower_threshold = 25
+    upper_threshold = 120
+    smoothing_neighbourhood = 3
+    sobel_size = 3
 
     img = cv2.imread(sys.argv[1])
 
-    smoothing_neighbourhood = max(3, smoothing_neighbourhood);
+    smoothing_neighbourhood = max(3, smoothing_neighbourhood)
     if not(smoothing_neighbourhood % 2):
-        smoothing_neighbourhood = smoothing_neighbourhood + 1;
+        smoothing_neighbourhood = smoothing_neighbourhood + 1
 
-    sobel_size = max(3, sobel_size);
+    sobel_size = max(3, sobel_size)
     if not(sobel_size % 2):
-        sobel_size = sobel_size + 1;
+        sobel_size = sobel_size + 1
 
-    # convert to grayscale
-
-    gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY);
+    gray_frame1 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY);
+    gray_frame2 = illuminationHSV(img)
 
     # performing smoothing on the image using a 5x5 smoothing mark (see manual entry for GaussianBlur())
 
-    smoothed = cv2.GaussianBlur(gray_frame,(smoothing_neighbourhood,smoothing_neighbourhood),0);
+    smoothed1 = cv2.GaussianBlur(gray_frame1, (smoothing_neighbourhood, smoothing_neighbourhood), 0)
+    smoothed2 = cv2.GaussianBlur(gray_frame2, (smoothing_neighbourhood, smoothing_neighbourhood), 0)
 
     # perform canny edge detection
 
-    canny = cv2.Canny(smoothed, lower_threshold, upper_threshold, apertureSize=sobel_size);
+    canny1 = cv2.Canny(smoothed1, lower_threshold, upper_threshold, apertureSize=sobel_size)
+    canny2 = cv2.Canny(smoothed2, lower_threshold, upper_threshold, apertureSize=sobel_size)
 
-    cv2.imshow(windowName,img);
-    cv2.moveWindow(windowName, 0, 100)
-    cv2.imshow(windowName2,canny)
-    cv2.moveWindow(windowName2, 640, 100)
+    #cv2.imshow(windowName, img)
+    cv2.imshow("Normal canny", canny1)
+    cv2.moveWindow("Normal canny", 0, 100)
+    cv2.imshow("HSV Value canny", canny2)
+    cv2.moveWindow("HSV Value canny", 640, 100)
 
     cv2.waitKey()
 
 else:
-    print("No image file specified.");
+    print("No image file specified.")
